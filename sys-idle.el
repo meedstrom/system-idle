809642c29a8d6cffba55f7c2eb26745bd14ca379
Martin Edström <meedstrom@runbox.eu>
2026-01-05 20:52:50 +0100

    Rename sys-idle -> system-idle 1/2

--- sys-idle.el
+++ sys-idle.el
@@ -1,4 +1,4 @@
-;;; sys-idle.el --- Get system-wide idle time  -*- lexical-binding: t; -*-
+;;; system-idle.el --- Poll the system-wide idle time  -*- lexical-binding: t; -*-
 ;; Copyright (C) 2026 Martin Edström
 
 ;; This program is free software: you can redistribute it and/or modify
@@ -17,12 +17,12 @@
 ;; Author:   Martin Edström <meedstrom91@gmail.com>
 ;; Created:  2026-01-05
 ;; Keywords: lisp
-;; URL:      https://github.com/meedstrom/sys-idle
+;; URL:      https://github.com/meedstrom/system-idle
 ;; Package-Requires: ((emacs "28.1"))
 
 ;;; Commentary:
 
-;; Call `sys-idle-seconds' to get the number of seconds since last user
+;; Call `system-idle-seconds' to get the number of seconds since last user
 ;; activity on the computer.
 
 ;; Supports (for now):
@@ -37,7 +37,7 @@
 
 ;; Test that it works:
 
-;; (run-with-timer 11 nil (lambda () (print (sys-idle-seconds))))
+;; (run-with-timer 11 nil (lambda () (print (system-idle-seconds))))
 
 ;;; Code:
 
@@ -46,41 +46,41 @@
 (declare-function dbus-get-property "dbus" (bus service path interface property))
 (require 'seq)
 
-(defun sys-idle--poll-mac ()
+(defun system-idle--poll-mac ()
   "Copy of `org-mac-idle-seconds'."
   (string-to-number
    (shell-command-to-string
     "ioreg -c IOHIDSystem | perl -ane 'if (/Idle/) {$idle=(pop @F)/1000000000; print $idle; last}'")))
 
-(defvar sys-idle--x11-program nil)
-(defun sys-idle--poll-x11 ()
+(defvar system-idle--x11-program nil)
+(defun system-idle--poll-x11 ()
   "Like `org-x11-idle-seconds'."
-  (unless sys-idle--x11-program
-    (error "sys-idle-seconds-x11: Install x11idle or xprintidle"))
+  (unless system-idle--x11-program
+    (error "system-idle-seconds-x11: Install x11idle or xprintidle"))
   (with-temp-buffer
-    (if (eq 0 (call-process sys-idle--x11-program nil t))
+    (if (eq 0 (call-process system-idle--x11-program nil t))
         (/ (string-to-number (buffer-string)) 1000)
-      (error "sys-idle-seconds-x11: %s failed: %s"
-             sys-idle--x11-program
+      (error "system-idle-seconds-x11: %s failed: %s"
+             system-idle--x11-program
              (buffer-string)))))
 
-(defvar sys-idle--dbus-session-path nil
+(defvar system-idle--dbus-session-path nil
   "Copy of `org-logind-dbus-session-path'.")
 
-(defun sys-idle--poll-elogind ()
+(defun system-idle--poll-elogind ()
   "Copy of `org-logind-user-idle-seconds'."
   (unless (fboundp 'dbus-get-property)
     (require 'dbus))
   (- (float-time)
      (/ (dbus-get-property :system
                            "org.freedesktop.login1"
-                           sys-idle--dbus-session-path
+                           system-idle--dbus-session-path
                            "org.freedesktop.login1.Session"
                            "IdleSinceHint")
         1e6)))
 
 ;; https://unix.stackexchange.com/questions/396911/how-can-i-tell-if-a-user-is-idle-in-wayland
-(defun sys-idle--poll-gnome ()
+(defun system-idle--poll-gnome ()
   "Check Mutter's idea of idle time, even on Wayland."
   (let* ((output (with-temp-buffer
                    (call-process "dbus-send" nil (current-buffer) nil
@@ -91,41 +91,41 @@
                    (buffer-string)))
          (idle-ms (if (string-match (rx space (+ digit) eol) output)
                       (string-to-number (match-string 0 output))
-                    (error "Function `sys-idle--poll-gnome' not working"))))
+                    (error "Function `system-idle--poll-gnome' not working"))))
     (/ idle-ms 1000.0)))
 
 ;; https://github.com/swaywm/swayidle/issues/147
 ;; https://github.com/swaywm/swayidle/issues/181
 ;; https://github.com/marvin1099/wayidletool
-(defvar sys-idle--swayidle-process nil)
-(defun sys-idle--ensure-swayidle ()
-  (when (not (process-live-p sys-idle--swayidle-process))
-    (setq sys-idle--swayidle-process
+(defvar system-idle--swayidle-process nil)
+(defun system-idle--ensure-swayidle ()
+  (when (not (process-live-p system-idle--swayidle-process))
+    (setq system-idle--swayidle-process
           (start-process-shell-command
            "swayidle"
-           " *sys-idle:swayidle*"
-           "swayidle timeout 9 'touch /tmp/sys-idle' resume 'rm /tmp/sys-idle'"))
-    (set-process-query-on-exit-flag sys-idle--swayidle-process nil)))
+           " *system-idle:swayidle*"
+           "swayidle timeout 9 'touch /tmp/system-idle' resume 'rm /tmp/system-idle'"))
+    (set-process-query-on-exit-flag system-idle--swayidle-process nil)))
 
-(defun sys-idle--poll-swayidle ()
+(defun system-idle--poll-swayidle ()
   "Check idle on compositors supporting the ext-idle-notify protocol.
 This includes KDE Plasma and Sway.
 Returns 0 if invoked during the first 9 seconds."
-  (sys-idle--ensure-swayidle)
-  (let ((attr (file-attributes "/tmp/sys-idle")))
+  (system-idle--ensure-swayidle)
+  (let ((attr (file-attributes "/tmp/system-idle")))
     (if attr
         (+ 9 (time-to-seconds
               (time-since (file-attribute-modification-time attr))))
       0)))
 
-(defvar sys-idle-seconds-function nil
-  "Function to be invoked by `sys-idle-seconds', must return a number.")
+(defvar system-idle-seconds-function nil
+  "Function to be invoked by `system-idle-seconds', must return a number.")
 
-(defun sys-idle-recalculate-variables (&optional assert)
-  "Try to set `sys-idle-seconds-function', emitting errors if ASSERT."
-  (setq sys-idle--x11-program
+(defun system-idle-recalculate-variables (&optional assert)
+  "Try to set `system-idle-seconds-function', emitting errors if ASSERT."
+  (setq system-idle--x11-program
         (seq-find #'executable-find '("x11idle" "xprintidle")))
-  (setq sys-idle--dbus-session-path
+  (setq system-idle--dbus-session-path
         (when (and (boundp 'dbus-runtime-version)
                    (require 'dbus nil t)
                    (member "org.freedesktop.login1" (dbus-list-activatable-names)))
@@ -136,34 +136,34 @@ Returns 0 if invoked during the first 9 seconds."
                               "org.freedesktop.login1.Manager"
                               "GetSessionByPID"
                               (emacs-pid)))))
-  (setq sys-idle-seconds-function
+  (setq system-idle-seconds-function
         (or (and (eq system-type 'darwin)
-                 #'sys-idle--poll-mac)
+                 #'system-idle--poll-mac)
             (let ((DESKTOP_SESSION (getenv "DESKTOP_SESSION")))
               (and DESKTOP_SESSION
                    (not (string-search "xorg" DESKTOP_SESSION))
                    (if (string-match-p (rx (or "gnome" "ubuntu")) DESKTOP_SESSION)
-                       #'sys-idle--poll-gnome
+                       #'system-idle--poll-gnome
                      (and (string-match-p (rx "plasma") DESKTOP_SESSION) ;; KDE
                           (if (executable-find "swayidle")
-                              (progn (sys-idle--ensure-swayidle)
-                                     #'sys-idle--poll-swayidle)
+                              (progn (system-idle--ensure-swayidle)
+                                     #'system-idle--poll-swayidle)
                             (when assert
-                              (error "sys-idle: Install swayidle")))))))
-            (and sys-idle--dbus-session-path
-                 #'sys-idle--poll-elogind)
+                              (error "system-idle: Install swayidle")))))))
+            (and system-idle--dbus-session-path
+                 #'system-idle--poll-elogind)
             ;; NOTE: This condition is also true under XWayland, so it must come
             ;; after all other checks for Wayland compositors if we want it to be
             ;; invoked under true X only.
             (and (eq window-system 'x)
-                 (if sys-idle--x11-program
-                     #'sys-idle--poll-x11
+                 (if system-idle--x11-program
+                     #'system-idle--poll-x11
                    (when assert
-                     (error "sys-idle: Install x11idle or xprintidle"))))
+                     (error "system-idle: Install x11idle or xprintidle"))))
             (when assert
-              (error "sys-idle: Could not get idle time on this system")))))
+              (error "system-idle: Could not get idle time on this system")))))
 
-(defun sys-idle-seconds ()
+(defun system-idle-seconds ()
   "Return the number of seconds the system has been idle.
 
 Unlike `current-idle-time', the result is intended to be correct even
@@ -172,13 +172,13 @@ user is still operating the computer but has a web browser or other
 application in focus.
 
 Always returns a number."
-  (unless sys-idle-seconds-function
-    (sys-idle-recalculate-variables t))
-  (let ((value (funcall sys-idle-seconds-function)))
+  (unless system-idle-seconds-function
+    (system-idle-recalculate-variables t))
+  (let ((value (funcall system-idle-seconds-function)))
     (if (numberp value)
         value
-      (error "Function at sys-idle-seconds-function did not return a number"))))
+      (error "Function at system-idle-seconds-function did not return a number"))))
 
-(provide 'sys-idle)
+(provide 'system-idle)
 
-;;; sys-idle.el ends here
+;;; system-idle.el ends here
